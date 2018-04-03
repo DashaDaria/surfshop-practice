7b4e19af992bbc21dc8edf63d6c075c1f875eab6
DashaDaria <dashadariallc@gmail.com>
2018-04-03 16:20:17 -0600

    created a fetch request to the rails api

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,26 +2,27 @@ import React, {Component} from 'react';
 import './App.css'
 import Surfboards from './Surfboards';
 
-const surfboards = [
-  {
-    name: "wind",
-    price: 55,
-    img_url: "https://images.evo.com/imgp/700/87599/401113/almond-surfboards-quadkumber-5-8-surfboard-gold-tint-top.jpg",
-    length: 33
-  },
-   {
-     name: "fire",
-     price: 66,
-     img_url: "https://images.evo.com/imgp/700/106869/449572/blackfern-6-0-bean-surfboard-6-0-front.jpg",
-     length: 44
-   }
-]
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      surfboards: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3001/api/surfboards')
+    .then(response => response.json())
+    .then(surfboards => this.setState({surfboards}))
+  }
+
   render(){
+    console.log(this.state)
     return (
       <div className="App">
-        <Surfboards surfboards={surfboards}/>
+        <Surfboards surfboards={this.state.surfboards}/>
       </div>
     )
   }
--- src/containers/Surfboards.js
+++ src/containers/Surfboards.js
@@ -1,14 +1,15 @@
-import React, {Component} from 'react';
+import React from 'react';
+import './Surfboards.css'
 
 const Surfboards = ({surfboards}) => (
-  <div>
+  <div className="SurfboardsContainer">
     <h1> Surfboards: </h1>
 
     {surfboards.map(surfboard =>
-      <div>
+      <div key={surfboard.id} className="Surfboardcard">
         <h3>{surfboard.name}</h3>
         <p>Price: ${surfboard.price}</p>
-        <img src={surfboard.img_url} alt={surfboard.name}/>
+        <img className="SurfboardImage" src={surfboard.img_url} alt={surfboard.name}/>
         <p>Length: {surfboard.length} inches</p>
       </div>
     )}
